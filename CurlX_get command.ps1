791ba378d56de042e4fcb712346a0e7d98b76963
jasonk91 <47259543+jasonk91@users.noreply.github.com>
2019-02-05 11:46:47 -0800

    Add files via upload

---
+++ CurlX_get command.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+Create a scheduled task on DCUTIL01 to execute a web service
+Written by Jason Kimberly.
+Version 1.1
+
+.LINK
+Insert Link here
+
+.DESCRIPTION 
+This is intended to run as a scheduled task on DCUTILS01 at 3:15am, every day. 
+
+#>
+
+curl -x PUT esytoolsapi02:3199/api/nullifyOldData
+
+Invoke-WebRequest put
