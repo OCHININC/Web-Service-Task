2c65cc184b57fa4894ab9e96c21eeeb97f93ef46
jasonk91 <47259543+jasonk91@users.noreply.github.com>
2019-02-05 12:00:29 -0800

    Update CurlX_get command.ps1

--- CurlX_get command.ps1
+++ CurlX_get command.ps1
@@ -9,7 +9,9 @@ Insert Link here
 
 .DESCRIPTION 
 This is intended to run as a scheduled task on DCUTILS01 at 3:15am, every day. 
-
+Calls a web service
+Put is an HTTP verb 
+No body means the request has no body (like parameters
 #>
 
 curl -x PUT esytoolsapi02:3199/api/nullifyOldData
