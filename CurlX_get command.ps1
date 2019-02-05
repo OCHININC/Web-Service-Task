17f325a0f4a754fb524402b8d365887b0cd9dbe6
jasonk91 <47259543+jasonk91@users.noreply.github.com>
2019-02-05 12:31:08 -0800

    Update CurlX_get command.ps1

--- CurlX_get command.ps1
+++ CurlX_get command.ps1
@@ -8,11 +8,14 @@ Version 1.1
 Insert Link here
 
 .DESCRIPTION 
-This is intended to run as a scheduled task on DCUTILS01 at 3:15am, every day. 
+This is intended to run as a scheduled task on DCUTILS01 at 3:15am, every day. Integrated API into smartsheet
 Calls a web service
 Put is an HTTP verb 
 No body means the request has no body (like parameters
+curl -x PUT esytoolsapi02:3199/api/nullifyOldData - original request
 #>
 
-curl -x PUT esytoolsapi02:3199/api/nullifyOldData
+
+
+Invoke-RestMethod -Method PUT -Uri http://esytoolsapi02:3199/api/nullifyOldData 
 
